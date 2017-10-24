008ed85c53b2b1717514b976b69d90a798b977e5
leolao <malenaantonio@yahoo.it>
2017-10-24 16:20:22 +0200

    tanti hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,24 @@
 
 public class Hello {
 	public static void main(){
-		System.out.println("hello wdgvvasfdb");
+		System.out.println("hello 0 perchè si parte sempre da 0");
+		System.out.println("hello 1");
+		System.out.println("hello 2");
+		System.out.println("hello 3");
+		System.out.println("hello 4");
+		System.out.println("hello 5");
+		System.out.println("hello 6");
+		System.out.println("hello 7");
+		System.out.println("hello 8");
+		System.out.println("hello 9");
+		System.out.println("hello 11");
+		System.out.println("hello 22");
+		System.out.println("hello 33");
+		System.out.println("hello 44");
+		System.out.println("hello 55");
+		System.out.println("hello 66");
+		System.out.println("hello 77");
+		System.out.println("hello 88");
+		System.out.println("hello 99");
 	}
 }
