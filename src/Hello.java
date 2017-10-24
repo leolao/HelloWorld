a49f08d7e655aec843d361cae537fe9c85378748
alemartani <33061142+alemartani@users.noreply.github.com>
2017-10-24 16:22:49 +0200

    boom

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,16 @@
 
 public class Hello {
 	public static void main(){
-		System.out.println("hello wdgvvasfdb");
+		System.out.println("hello 10");
+		System.out.println("hello 9");
+		System.out.println("hello 8");
+		System.out.println("hello 7");
+		System.out.println("hello 6");
+		System.out.println("hello 5");
+		System.out.println("hello 4");
+		System.out.println("hello 3");
+		System.out.println("hello 2");
+		System.out.println("hello 1");
+		System.out.println("BOOM!");
 	}
 }
